3acb5e676d5917680b8672c7bd2b519fa2ff24a0
khan-shuvo <shuvo2004@outlook.com>
2025-10-19 14:51:36 +0600

    accept path as props and implement in like

--- src/components/HeaderItems.jsx
+++ src/components/HeaderItems.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export default function HeaderItems({name, Icon}) {
+export default function HeaderItems({ name, Icon, path }) {
   return (
-    <div className='text-white flex items-center gap-3 text-[15px] font-semibold cursor-pointer'>
-        <Icon/>
+    <Link to={path}>
+      <div className='text-white flex items-center gap-3 text-[15px] font-semibold cursor-pointer'>
+        <Icon />
         <h1 className='relative after:absolute after:content-[""] after:w-0 after:-bottom-1 after:h-[2px] after:left-0 after:bg-white after:transition-all after:duration-300 hover:after:w-full'>{name}</h1>
-    </div>
+      </div>
+    </Link>
   )
 }
