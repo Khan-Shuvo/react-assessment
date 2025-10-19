95b9230d828b36b5817a1ec9fa8564d1451d6016
khan-shuvo <shuvo2004@outlook.com>
2025-10-19 18:56:02 +0600

    link card componet for show individual movie

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 const Image_Base_Url = import.meta.env.VITE_TMDB_IMAGE_URL
 
-export default function MovieCard({movies}) {
+export default function MovieCard({ movies }) {
   return (
-    <div className='hover:border hover:border-gray-400 hover:rounded-md hover:shadow-md hover:scale-105 transition-all duration-200 flex items-center'>
+    <Link to = {`/movie/${movies.id}`}>
+      <div className='hover:border hover:border-gray-400 hover:rounded-md hover:shadow-md hover:scale-105 transition-all duration-200 flex items-center'>
         <img src={Image_Base_Url + movies.poster_path} className='rounded-md ' />
-    </div>
+      </div>
+    </Link>
   )
 }
