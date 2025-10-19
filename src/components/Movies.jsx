1374f93380586e968974c1fb46570581b0e45608
khan-shuvo <shuvo2004@outlook.com>
2025-10-19 15:24:08 +0600

    this components show all genres movies

---
+++ src/components/Movies.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import GenresList from '../Constant/GenresList'
+import MovieList from './MovieList'
+
+export default function Movies() {
+  return (
+    <div>
+        {
+            GenresList.genere.map(item => (
+                <div className='px-8 md:p-10'>
+                    <h2 className=' text-[20px] md:text-3xl text-white font-bold py-3'> 
+                        {item.name}
+                    </h2>
+                    <MovieList genreId={item.id}/>
+                </div>
+            ))
+        }
+    </div>
+  )
+}
