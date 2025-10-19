158f1f7d4a04972d2d899a6593aec06f3811590e
khan-shuvo <shuvo2004@outlook.com>
2025-10-19 18:53:55 +0600

    change some class for h1

--- src/components/HeaderItems.jsx
+++ src/components/HeaderItems.jsx
@@ -6,7 +6,7 @@ export default function HeaderItems({ name, Icon, path }) {
     <Link to={path}>
       <div className='text-white flex items-center gap-3 text-[15px] font-semibold cursor-pointer'>
         <Icon />
-        <h1 className='relative after:absolute after:content-[""] after:w-0 after:-bottom-1 after:h-[2px] after:left-0 after:bg-white after:transition-all after:duration-300 hover:after:w-full'>{name}</h1>
+        <h1 className='relative md:text-xl text-[12px] uppercase after:absolute after:content-[""] after:w-0 after:-bottom-1 after:h-[2px] after:left-0 after:bg-white after:transition-all after:duration-300 hover:after:w-full'>{name}</h1>
       </div>
     </Link>
   )
