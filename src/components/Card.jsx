e4d439e6f3e7cb4375efa6d53c176a8f28071816
khan-shuvo <shuvo2004@outlook.com>
2025-10-25 21:24:35 +0600

    verify the type and export the type in path

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,8 +3,10 @@ import { Link } from 'react-router-dom'
 const Image_Base_Url = import.meta.env.VITE_TMDB_IMAGE_URL
 
 export default function Card({ movies }) {
+
+  const type = movies.title? "movie":"tv"
   return (
-    <Link to = {`/movie/${movies.id}`}>
+    <Link to = {`/${type}/${movies.id}`}>
       <div className='hover:border hover:border-gray-400 hover:rounded-md hover:shadow-md hover:scale-105 transition-all duration-200 flex items-center'>
         <img src={Image_Base_Url + movies.poster_path} className='rounded-md ' />
       </div>
