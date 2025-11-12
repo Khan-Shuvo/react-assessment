f3ecfc0554a41a603a9ee430c7154b307cede2bb
khan-shuvo <shuvo2004@outlook.com>
2025-11-12 18:15:43 +0600

    make slider by swiper

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,14 +1,15 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { getTrendingVideos } from '../api/tmdb'
-import { HiChevronLeft, HiChevronRight } from 'react-icons/hi2';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination, Autoplay } from "swiper/modules";
 
 const IMAGE_BASE_URL = import.meta.env.VITE_TMDB_IMAGE_URL;
-const screenWidt = window.innerWidth;
 
 export default function Slider() {
 
     const [movies, setMovies] = useState([]);
-    const elementref = useRef()
 
     useEffect(() => {
         async function fetchMovies() {
@@ -24,23 +25,31 @@ export default function Slider() {
 
     }, []);
 
-    const sliderRight = (element) => {
-        element.scrollLeft += screenWidt;
-    };
-    const sliderLeft = (element) => {
-        element.scrollLeft -= screenWidt;
-    };
 
     return (
-        <div>
-            <HiChevronLeft className='hidden md:block text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer' onClick={() => sliderLeft(elementref.current)} />
-            <HiChevronRight className='hidden md:block text-white text-[30px] absolute mx-8 mt-[150px]  right-0 cursor-pointer' onClick={() => sliderRight(elementref.current)} />
-            <div className='flex overflow-x-auto w-full px-16 py-4scrollbar-none scroll-smooth' ref={elementref}>
-                {movies.map((item) => (
-                    <img src={IMAGE_BASE_URL + item.backdrop_path}
-                        className='min-w-full  md:h-[310px] object-cover object-left-top mr-5 rounded-md hover:border-[4px] border-gray-400 transition-all duration-100 ease-in' />
+        <div className="w-full">
+            <Swiper
+                modules={[Pagination, Autoplay]}
+                pagination={{ dynamicBullets: true }}
+                autoplay={{
+                    delay: 3000, // ⏱️ 3 seconds
+                    disableOnInteraction: false, // keeps autoplay even after user interaction
+                }}
+                spaceBetween={0}
+                slidesPerView={1}
+                loop={true} // makes the slider loop infinitely
+                className="mySwiper w-full h-[60vh] md:h-[70vh] lg:h-[80vh] "
+            >
+                {movies.map((item, index) => (
+                    <SwiperSlide key={index}>
+                        <img
+                            src={IMAGE_BASE_URL + item.backdrop_path}
+                            alt={`Slide ${index + 1}`}
+                            className="w-full h-full object-cover"
+                        />
+                    </SwiperSlide>
                 ))}
-            </div>
+            </Swiper>
         </div>
     )
 }
